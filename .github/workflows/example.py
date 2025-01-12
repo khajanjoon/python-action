706205a5a508f9c9bd03ca5934d76debed17ece2
khajanjoon <48195154+khajanjoon@users.noreply.github.com>
2025-01-12 13:12:40 +0530

    Update example.py

--- .github/workflows/example.py
+++ .github/workflows/example.py
@@ -1,15 +1,46 @@
 import requests
 import asyncio
 import json
+import os
+import hashlib
+import hmac
+import time
+import datetime
+import hashlib
+import hmac
+import base64
+from decimal import Decimal
 
-Authorization = '7ygBbjKWiBdB7XwblKVKnZfMFj1dnz458qNiizq10chfKU0Mhf1'
+
+api_key = 'WqLMWdFHsYrWt5dHELyBkZXzVw54s4'
+api_secret = 'ux8394Juap4ZzvA3oXPYkgVaR4MAza6BwsKWLTOVCFNcv5wgPi3HAb0Pqirm'
+
+def generate_signature(method, endpoint, payload):
+    timestamp = str(int(time.time()))
+    signature_data = method + timestamp + endpoint + payload
+    message = bytes(signature_data, 'utf-8')
+    secret = bytes(api_secret, 'utf-8')
+    hash = hmac.new(secret, message, hashlib.sha256)
+    return hash.hexdigest(), timestamp
+
+def get_time_stamp():
+    d = datetime.datetime.utcnow()
+    epoch = datetime.datetime(1970,1,1)
+    return str(int((d - epoch).total_seconds()))
+
+
+
+
+
+
+BOT_TOKEN = '7003653511:AAGkx1MumC07d4gJh9zb9l7dCqDfyeTHjtY'
+# Replace 'YOUR_CHAT_ID' with the chat ID you want to send the message to
+CHAT_ID = '311396636'
 
 async def fetch_profile_data():
-    while True:
-
-        requests.post("https://ntfy.sh/buy_algo",
-        data="Buy Algo Live😀".encode(encoding='utf-8'))
-        await asyncio.sleep(300)
+    
+   print("response")
+   send_message("Algo Start")    
 
 async def place_target_order(order_type,side,order_product,order_size,stop_order_type,stop_price):
     # Define the payload
@@ -23,15 +54,21 @@ async def place_target_order(order_type,side,order_product,order_size,stop_order
         "stop_trigger_method": "mark_price",
         "size": order_size
     }
-    print(payload)
     # Fetch data from REST API
-    
+    # Fetch data from REST API
+    method = 'POST'
+    endpoint = '/v2/orders'
+    payload_str = json.dumps(payload)
+    signature, timestamp = generate_signature(method, endpoint, payload_str)
+    timestamp = get_time_stamp() 
 
     headers = {
-      'Authorization': Authorization, 
-      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4.1 Safari/605.1.15',
-      'Content-Type': 'application/json'
-    }
+         'api-key': api_key,
+         'timestamp': timestamp,
+         'signature': signature,
+         'User-Agent': 'rest-client',
+         'Content-Type': 'application/json'
+           }
 
     # Send the POST request with the payload
     response = requests.post('https://cdn.india.deltaex.org/v2/orders', json=payload, headers=headers)
@@ -39,8 +76,16 @@ async def place_target_order(order_type,side,order_product,order_size,stop_order
     # Check if the request was successful
     if response.status_code == 200:
         print("Order placed successfully.")
-        requests.post("https://ntfy.sh/buy_trade",
-        data="order successful 😀".encode(encoding='utf-8'))
+        message = f"😀New Order:\n" \
+          f"Order Type: {payload['order_type']}\n" \
+          f"Side: {payload['side']}\n" \
+          f"Product ID: {payload['product_id']}\n" \
+          f"Stop Order Type: {payload['stop_order_type']}\n" \
+          f"Stop Price: {payload['stop_price']}\n" \
+          f"Reduce Only: {payload['reduce_only']}\n" \
+          f"Stop Trigger Method: {payload['stop_trigger_method']}\n" \
+          f"Size: {payload['size']}😀"
+        send_message(message)
     else:
         print("Failed to place order. Status code:", response.status_code)
 
@@ -56,41 +101,61 @@ async def place_order(order_type,side,order_product_id,order_size,stop_order_typ
     }
     
     # Fetch data from REST API
+    method = 'POST'
+    endpoint = '/v2/orders'
+    payload_str = json.dumps(payload)
+    signature, timestamp = generate_signature(method, endpoint, payload_str)
+    timestamp = get_time_stamp() 
     
 
     headers = {
-      'Authorization': Authorization, 
-      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4.1 Safari/605.1.15',
-      'Content-Type': 'application/json'
-    }
-
+         'api-key': api_key,
+         'timestamp': timestamp,
+         'signature': signature,
+         'User-Agent': 'rest-client',
+         'Content-Type': 'application/json'
+           }
     # Send the POST request with the payload
     response = requests.post('https://cdn.india.deltaex.org/v2/orders', json=payload, headers=headers)
     
     # Check if the request was successful
     if response.status_code == 200:
-        print("Order placed successfully.")
-        requests.post("https://ntfy.sh/buy_trade",
-        data="order successful 😀".encode(encoding='utf-8'))
+        message = f"😀New Order:\n" \
+          f"Order Type: {payload['order_type']}\n" \
+          f"Side: {payload['side']}\n" \
+          f"Product ID: {payload['product_id']}\n" \
+          f"Reduce Only: {'Yes' if payload['reduce_only'] else 'No'}\n" \
+          f"Size: {payload['size']}😀"
+        send_message(message)
         await place_target_order("market_order","sell",order_product_id,1,"take_profit_order",target_value )
     else:
-        print("Failed to place order. Status code:", response.status_code)
-      
+        send_message(response)
 
 async def fetch_position_data():
     while True:
         # Fetch data from REST API
        
+        payload = ''
+        method = 'GET'
+        
+        method = 'GET'
+        endpoint = '/v2/positions/margined'
+        payload_str = json.dumps(payload)
+        signature, timestamp = generate_signature(method, endpoint, payload)
+        timestamp = get_time_stamp() 
 
         headers = {
-          'Authorization': Authorization, 
-          'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4.1 Safari/605.1.15',
-          'Content-Type': 'application/json'
-        }
+         'api-key': api_key,
+         'timestamp': timestamp,
+         'signature': signature,
+         'User-Agent': 'rest-client',
+         'Content-Type': 'application/json'
+  }
 
         r = requests.get('https://cdn.india.deltaex.org/v2/positions/margined', headers=headers)
         position_data = r.json()  # Extract JSON data using .json() method
         #print("Position Data:", position_data)
+        send_message("Algo Live") 
         # Extract product_id and realized_pnl from each result
         # Extract data from each dictionary in the 'result' list
         for result in position_data["result"]:
@@ -105,31 +170,40 @@ async def fetch_position_data():
            user_id = result["user_id"]
            entry_price = result["entry_price"]
            mark_price = result["mark_price"]
+          
            # Print the extracted data
-           print("Product ID:", product_id, 
-            "Product Symbol:", product_symbol, 
-            "Realized Cashflow:", realized_cashflow, 
-            "Realized Funding:", realized_funding, 
-            "Realized PnL:", realized_pnl, 
-            "Size:", size, 
-            "Unrealized PnL:", unrealized_pnl, 
-            "Updated At:", updated_at, 
-            "User ID:", user_id, 
-            "entry_price:", entry_price, 
-            "mark_price:", mark_price)
+           
 
            print()  # Add an empty line for better readability between each dictionary's data
 
            # Percentage of entry price
            percentage = int(size)*.75 # Assuming 10% for demonstration purposes
            price_value = float(entry_price)-(float(entry_price) * (percentage / 100)) 
-           tick_size = 0.05
-           target = float(entry_price)*2/100+float(entry_price)
-           target_value = round(target / tick_size) * tick_size
-           print(price_value)
-           print(target_value)  # Add an empty line for better readability between each dictionary's data
+           digit_count = count_digits_after_point(mark_price)
+           #print(digit_count)
+           tick_size = 1/digit_count
+           #print(tick_size)
+           target = float(mark_price)*2/100+float(mark_price)
+           number  = round((target / tick_size) * tick_size,digit_count)
+           # Example usage
+           target_value=number
+           decimal_number = scientific_to_decimal(number)
+           
+         
+           
+           message = f"Symbol: {product_symbol}\n" \
+          f"Size: {size}\n" \
+          f"Unrealized PnL: {round((float(unrealized_pnl) ), digit_count) }\n" \
+          f"Entry Price: {round((float(entry_price) ), digit_count) }\n" \
+          f"Next_Entry: {round((float(price_value) ), digit_count) }\n" \
+          f"Mark Price: {round((float(mark_price) ), digit_count) }\n"  \
+          f"target_value: {round((float(decimal_number) ), digit_count) }\n"
+          
+           print(message) 
+           #send_message(message)
+            # Add an empty line for better readability between each dictionary's data
            if (float(mark_price) < price_value) :
-
+            
             print("ready to buy")
             print()  # Add an empty line for better readability between each dictionary's data
             await place_order("market_order","buy",product_id,1,0,target_value )  
@@ -138,13 +212,65 @@ async def fetch_position_data():
         # Wait for 60 seconds before fetching again
         await asyncio.sleep(30)
 
-async def main():
+def count_digits_after_point(number):
+    # Convert the number to a string
+    number_str = str(number)
+    
+    # Split the string at the decimal point
+    parts = number_str.split('.')
+    
+    # Check if there is a decimal part
+    if len(parts) == 2:
+        # Return the length of the decimal part
+        return len(parts[1])
+    else:
+        # Return 0 if there is no decimal part
+        return 0
 
-    # Run profile data fetching coroutine
-    profile_task = asyncio.create_task(fetch_profile_data())
-    position_task = asyncio.create_task(fetch_position_data())
-    # Wait for both tasks to complete
-    await asyncio.gather(position_task, profile_task)
+def scientific_to_decimal(number):
+    # Convert the number to a Decimal and return as string
+    decimal_number = Decimal(number)
+    return str(decimal_number)
+
+def send_message(message):
+    url = f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage'
+    params = {'chat_id': CHAT_ID, 'text': message}
+
+    response = requests.post(url, json=params)
+    if response.status_code == 200:
+        print('Message sent successfully!')
+    else:
+        print(f'Failed to send message. Error: {response.status_code} - {response.text}')
+
+def auto_topup(message):
+    headers = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'api-key': '****',
+      'signature': '****',
+      'timestamp': '****'
+      }
+
+    r = requests.put('https://api.delta.exchange/v2/positions/auto_topup', params={
+    "product_id": 0,
+    "auto_topup": "false"
+    }, headers = headers)
+    print(r.json())
+
+
+async def main():
+    while True:
+        try:
+            profile_task = asyncio.create_task(fetch_profile_data())
+            position_task = asyncio.create_task(fetch_position_data())
+            await asyncio.gather(position_task, profile_task)
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            # Optionally, you can add code here to handle the error, such as logging it
+            # or sending a notification
+        finally:
+            # Optionally, you can add a delay here before retrying
+            await asyncio.sleep(10)
     
 
 # Run the main coroutine
